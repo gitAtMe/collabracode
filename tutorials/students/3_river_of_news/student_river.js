bc9b4b2fb3fc67f12a0efbde7ef801b23bf8c442
gitAtMe <Account@.(none)>
2011-04-25 22:20:12 -0400

    Worked on the river of news project...

--- tutorials/students/3_river_of_news/student_river.js
+++ tutorials/students/3_river_of_news/student_river.js
@@ -1,7 +1,35 @@
 var riverCallback = {
   "onsuccess" : function(obj) {
-    // TODO
-    log("SUCCESS: Loaded rss.json file");
+
+	var items = obj.rss.channel.item;
+	var river = document.getElementById('river');
+
+	for(var key in items){
+	  var p = document.createElement('p');
+	  var h3 = document.createElement('h3');
+	  var txt = document.createTextNode(items[key].title);
+	  var span = document.createElement('span');
+
+	  h3.appendChild(txt);
+	  addEvent(h3,'click',function(){
+		var s = this.nextSibling;
+
+		if(s.className=='hidden')
+		  s.className = '';
+		else
+		  s.className = 'hidden';
+	  });
+
+	  span.className = 'hidden';
+	  span.innerHTML = items[key].description.replace(/&quot;/g,'"');
+	  p.appendChild(h3);
+	  p.appendChild(span);
+
+	  river.appendChild(p);
+//	  log(items[key]);		
+	}
+
+//    log("SUCCESS: Loaded rss.json file");
   },
   "onerror" : function(req) {
     log("ERROR: Unable to download rss.json file" + req);
